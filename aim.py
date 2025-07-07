9cdfcfad96f89d71dcc5bc1cabff926ac1ffa99e
Johntenn44 <johntenn44@gmail.com>
2025-07-07 17:09:49 +0100

    Update aim.py

--- aim.py
+++ aim.py
@@ -8,6 +8,7 @@ import ccxt
 import pandas as pd
 import numpy as np
 from datetime import datetime
+import flask 
 
 # Start webserver.py subprocess for Render port binding
 webserver_path = os.path.join(os.path.dirname(__file__), 'webserver.py')
